872d69ecac897b359b852235736411761e5b9048
hhHhh15423 <131933219+hhHhh15423@users.noreply.github.com>
2023-05-06 19:43:00 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,9 +14,25 @@ def ridge(data):
     return weight @ data
     
 def lasso(data):
-    X,y = read_data()
-    weight = np.matmul(np.linalg.inv(np.matmul(X.T,X)),np.matmul(X.T,y))
-    return weight @ data
+    mu = 1e-2;
+opts = struct();
+opts.method = 'grad_huber';
+opts.verbose = 0;
+opts.maxit = 4000;
+opts.ftol = 1e-8;
+opts.alpha0 = 1 / L;
+[x, out] = LASSO_con(x0, A, b, mu, opts);
+f_star = min(out.fvec);
+
+opts.verbose = 0;
+opts.maxit = 400;
+if opts.verbose
+    fprintf('\nmu=1e-2\n');
+end
+[x, out] = LASSO_con(x0, A, b, mu, opts);
+data2 = (out.fvec - f_star)/f_star;
+k2 = min(length(data2),400);
+data2 = data2(1:k2);
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
